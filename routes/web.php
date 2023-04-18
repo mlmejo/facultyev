7180c3b62459c519d51c261ef251302f6b1b033f
mlmejo <mlmejo12@gmail.com>
2023-04-19 04:43:21 +0800

    Change index page

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/register/admin')
+    ->middleware('guest');
 
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
